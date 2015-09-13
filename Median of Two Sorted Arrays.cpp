d22374191607eb9c57ae4910446043221c77c761
ztongfighton <ztong@usc.edu>
2015-09-13 11:51:44 +0800

    Update Median of Two Sorted Arrays.cpp

--- Median of Two Sorted Arrays.cpp
+++ Median of Two Sorted Arrays.cpp
@@ -4,9 +4,10 @@ public:
         const int m = nums1.size();
         const int n = nums2.size();
         const int l = m + n;
-        int *n1 = nums1.data();
+        int *n1 = nums1.data();    //use data() function to access memory array internally used in vector
         int *n2 = nums2.data();
-        if (l % 2 == 1) return findKth(n1, m, n2, n, l / 2 + 1);
+        //indexing of k. k shall be indexed starting from 1 and such indexing consistency shall be kept throughout the program
+        if (l % 2 == 1) return findKth(n1, m, n2, n, l / 2 + 1);   
         else return (findKth(n1, m, n2, n, l / 2) + findKth(n1, m, n2, n, l / 2 + 1)) / 2.0;
     }
 
@@ -14,7 +15,7 @@ private:
     int findKth(int *nums1, int m, int *nums2, int n, int k){
         if (m > n) return findKth(nums2, n, nums1, m, k);
         if (m == 0) return nums2[k - 1];
-        if (k == 1) return min(nums1[0], nums2[0]);
+        if (k == 1) return min(nums1[0], nums2[0]);  //A termination case
         int a = min(m, k / 2);
         int b = k - a;
         if (nums1[a - 1] == nums2[b - 1]) return nums1[a - 1];
