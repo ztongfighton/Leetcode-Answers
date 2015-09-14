dcf554df490287f1e4d89edf32c95dce07902d71
ztongfighton <ztong@usc.edu>
2015-09-14 12:09:09 +0800

    Update Gray Code.cpp

--- Gray Code.cpp
+++ Gray Code.cpp
@@ -1,27 +1,15 @@
 class Solution {
 public:
     vector<int> grayCode(int n) {
-        vector<int> prev;
-        prev.push_back(0);
-        if (n < 1) return prev;
-        vector<int> cur;
-        prev.push_back(1);
-        for (int i = 2; i <= n; ++i){
-            bool flag = true;
-            for (int j = 0; j < prev.size(); ++j){
-                if(flag){
-                    cur.push_back(prev[j] * 2);
-                    cur.push_back(prev[j] * 2 + 1);
-                }
-                else{
-                    cur.push_back(prev[j] * 2 + 1);
-                    cur.push_back(prev[j] * 2);
-                }
-                flag = !flag;
-            }
-            swap(cur, prev);
-            cur.clear();
+        vector<int> result;
+        const int len = 1 << n;
+        result.reserve(len);
+        result.push_back(0);
+        for (int i = 0; i < n; ++i){
+            int highest_bit = 1 << i;
+            for (int j = result.size() - 1; j >= 0; --j)
+                result.push_back(highest_bit | result[j]);
         }
-        return prev;
+        return result;
     }
 };
