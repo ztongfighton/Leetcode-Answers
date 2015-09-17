783d507745ce0ad3e0e51c56dbdd4a8ad20c5a1e
ztongfighton <ztong@usc.edu>
2015-09-17 11:06:31 +0800

    Update Binary Tree Inorder Traversal.cpp

--- Binary Tree Inorder Traversal.cpp
+++ Binary Tree Inorder Traversal.cpp
@@ -7,6 +7,8 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+ 
+//This is the Morris traversal version
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
